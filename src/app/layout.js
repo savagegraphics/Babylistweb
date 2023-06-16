dbfa5120410e07942e59d5c1caa38f39943685ee
savagegraphics <olawaleraji188@gmail.com>
2023-06-15 19:22:29 -0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,10 @@
 "use client";
-import { metadata } from './metadata';
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { BrowserRouter } from 'react-router-dom'
 
 const inter = Inter({ subsets: [ 'latin' ] })
 
-export { metadata };
 
 export default function RootLayout({ children }) {
   return (
